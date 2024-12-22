44a751dfc7d1208c2f24494a36f891898e045b41
Anastasiya <165375443+anastasiya-red@users.noreply.github.com>
2024-12-23 01:51:02 +0300

    Update module_11_1_библиотека_pillow.py

--- module_11_1_библиотека_pillow.py
+++ module_11_1_библиотека_pillow.py
@@ -4,10 +4,10 @@ image = Image.open('name image.jpg')
 print(image.format, image.size, image.mode)
 
 r, g, b = image.split()
-image = Image.merge("RGB", (g, r, b))
+image = Image.merge("RGB", (b, r, r))
 
-image = image.resize((500, 700)) #изменение размера
-image = image.rotate(45)# поворот изображения
+image = image.resize((500, 600)) #изменение размера
+image = image.rotate(180)# поворот изображения
 
 image.show()
 image.save('new image.png')
